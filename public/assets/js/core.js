df180713183fe7ea32478331e061675a30d1f024
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2015-12-15 18:37:02 +0100

    simple lights

--- public/assets/js/core.js
+++ public/assets/js/core.js
@@ -1,21 +1,112 @@
+Vue.config.debug = true;
+
 var config = {
-    CHANGE_LIGHTS_URL: 'http://localhost:8081/api/baby'
+    LIGHTS: 'http://192.168.1.141/light',
+    LIGHT_UP: 'http://192.168.1.141/light_up',
+    LIGHT_DOWN: 'http://192.168.1.141/light_down',
+    // LIGHT_INTENSITIVITY: 'http://192.168.1.141/p_light'
+    LIGHT_INTENSITIVITY: 'assets/mocks/light-intensivity.html',
+    LIGHT_MEASURE: 'assets/mocks/light-intensivity.html',
+    TEMP: 'assets/mocks/light-intensivity.html',
+    AIR_HUMIDITY: 'assets/mocks/light-intensivity.html'
+
 };
 
 new Vue({
-    el: '#toggles',
+    el: '#lightToggles',
+
+    data: {
+        lightSensitivity: 0
+    },
 
     methods: {
 
-        changeLightsState: function() {
-            console.log('aa');
-            this.$http.get(config.CHANGE_LIGHTS_URL)
+        lights: function() {
+            this.$http.get(config.LIGHTS)
                 .success(function(res){
                     console.log(res);
                 })
                 .error(function(err){
                     console.log(err);
                 });
+        },
+
+        lightsUp: function() {
+            this.$http.get(config.LIGHT_UP)
+                .success(function(res){
+                    console.log(res);
+
+                    this.getLightsIntensivity();
+                })
+                .error(function(err){
+                    console.log(err);
+                });
+        },
+
+        lightsDown: function() {
+            this.$http.get(config.LIGHT_DOWN)
+                .success(function(res){
+                    console.log(res);
+
+                    this.getLightsIntensivity();
+                })
+                .error(function(err){
+                    console.log(err);
+                });
+        },
+
+        getLightsIntensivity: function() {
+            this.$http.get(config.LIGHT_INTENSITIVITY)
+                .success(function(res){
+
+                    this.lightSensitivity = parseInt(res);
+                })
+                .error(function(err){
+                    console.log(err);
+                });
         }
+    },
+
+    ready: function() {
+        this.getLightsIntensivity();
+    }
+});
+
+
+var statusTable = new Vue({
+    el: '#statusTable',
+    data: {
+        lightIntensity: 0,
+        temp: 0,
+        humidity: 0
+    },
+
+    ready: function() {
+        this.$http.get(config.LIGHT_MEASURE)  // @todo
+            .success(function(res){
+
+                this.lightIntensity = parseInt(res);
+            })
+            .error(function(err){
+                console.log(err);
+            });
+
+        this.$http.get(config.TEMP) // @todo
+            .success(function(res){
+
+                this.temp = parseInt(res);
+            })
+            .error(function(err){
+                console.log(err);
+            });
+
+        this.$http.get(config.AIR_HUMIDITY) // @todo
+            .success(function(res){
+
+                this.humidity = parseInt(res);
+            })
+            .error(function(err){
+                console.log(err);
+            });
     }
 });
